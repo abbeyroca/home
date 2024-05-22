57651d31b9fb2880ac2cef04481c8f85c1f3404e
Luisa Burini <luisa.burini@gmail.com>
2024-05-21 22:03:22 -0300

    Changes to be committed: 	deleted:    calendar.js 	modified:   index.html 	new file:   js/calendar.js 	new file:   js/eventos.js

---
+++ js/calendar.js
@@ -0,0 +1,11 @@
+import { eventos } from './eventos.js';
+
+console.log("Eventos " + eventos())
+
+for (const [key, value] of Object.entries(eventos())) {
+    console.log(key, value);
+    const para = document.createElement("p");
+    para.innerText = value['data'] + " " + key;
+    var el = document.getElementById("calendar") 
+    el.appendChild(para);
+  }
---
+++ js/eventos.js
@@ -0,0 +1,17 @@
+export function eventos() {
+    return new Object({
+        'Abbey Indie Fest': {
+            data: '11/05/2024',
+            descricao: 'Abbey Indie Fest'
+        },
+        'Arraiabbey': {
+            data: '08/06/2024',
+            descricao: 'Arraiabbey'
+        },
+        'Evento 2': {
+            data: '20/07/2024',
+            descricao: 'Evento 2'
+        },
+    })
+}
+

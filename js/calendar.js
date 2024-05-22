eaa4707fd0d5a0fc6b8bfb5b3f9fbfc37eb65db4
Luisa Burini <luisa.burini@gmail.com>
2024-05-21 22:49:28 -0300

    renamed:    imgs/logo abbey.jpeg -> imgs/enxada_abbey.jpeg new file:   imgs/logo.jpeg modified:   index.html modified:   js/calendar.js modified:   style/abbey.css

--- js/calendar.js
+++ js/calendar.js
@@ -1,11 +1,13 @@
 import { eventos } from './eventos.js';
 
-console.log("Eventos " + eventos())
+function updateCalendar() {
+  console.log("Eventos " + eventos())
 
-for (const [key, value] of Object.entries(eventos())) {
-    console.log(key, value);
-    const para = document.createElement("p");
-    para.innerText = value['data'] + " " + key;
-    var el = document.getElementById("calendar") 
-    el.appendChild(para);
-  }
+  for (const [key, value] of Object.entries(eventos())) {
+      console.log(key, value);
+      const para = document.createElement("p");
+      para.innerText = value['data'] + " " + key;
+      var el = document.getElementById("calendar") 
+      el.appendChild(para);
+    }
+}
